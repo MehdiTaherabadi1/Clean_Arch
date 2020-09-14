a8a162521f1577cc47f64101275119f734bf701f
Mehdi Taherabadi <MehdiTaherabadi@outlook.com>
2020-09-14 15:58:15 +0430

    Add Course

---
+++ Clean_Arch/CleanArch.Domin/Models/Course/Course.cs
@@ -0,0 +1,14 @@
+﻿namespace CleanArch.Domin.Models.Course
+{
+    public class Course
+    {
+        #region propertios
+
+        public int  CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string Descerption { get; set; }
+        public string ImageUrl { get; set; }
+
+        #endregion
+    }
+}
