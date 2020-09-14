6d3adc6e961913abac0bc9ba9b6af271d1202a21
Mehdi Taherabadi <MehdiTaherabadi@outlook.com>
2020-09-14 16:15:22 +0430

    Create Project Context

---
+++ Clean_Arch/CleanArch.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,19 @@
+﻿using CleanArch.Domin.Models.Course;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class UniversityDBContext : DbContext
+    {
+        #region construtor
+
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options):base(options)
+        {
+            
+        }
+
+        #endregion
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
